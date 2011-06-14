383e45355fe5c5271efc8f382b6ff8a792926311
Takahiro Kamatani <buhii314@gmail.com>
2011-06-15 06:58:36 +0900

    Added README.rst's detail and setup.py's properties

--- README.rst
+++ README.rst
@@ -4,5 +4,19 @@ Tomato
 Python SWF Processor (for Flash Lite 1.1)
 -----------------------------------------
 
+`Tomato` is a package for processing SWF files.
+
+Requirements
+------------
+* Python 2.6 or greater
+* PIL 1.1.7 or greater
+* bitarray 0.3.5 or greater
+* msgpack 0.1.8 or greater
+
+GitHub
+------
 http://github.com/buhii/tomato
 
+Documentation(Japanese)
+-----------------------
+http://buhii.github.com/tomato/
--- setup.py
+++ setup.py
@@ -3,21 +3,35 @@ import sys, os
 
 version = '0.0.1'
 
+# See: http://packages.python.org/an_example_pypi_project/setuptools.html
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 setup(name='tomato',
       version=version,
       description="Python SWF Processor (for Flash Lite 1.1)",
-      long_description="Python SWF Processor (for Flash Lite 1.1)",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
+      long_description=read("README.rst"),
+      keywords='SWF Flash Lite 1.1',
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Topic :: Multimedia",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
       author='Takahiro Kamatani',
-      url='https://github.com/buhii/tomato.git',
+      author_email='buhii314@gmail.com',
+      url='https://github.com/buhii/tomato',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        "msgpack-python",
+        "msgpack-python>=0.1.8, !=015final",
         "bitarray",
+        "PIL",
       ],
       entry_points="""
       # -*- Entry points: -*-
